69de8ada997fad6ee44acf6b3ad3c685313c0c53
genialpleasure <isaacnpleasure01@gmail.com>
2025-10-15 23:55:09 +0100

    using disabled prop

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,26 @@ import Player from './Components/Player.jsx';
 import GameBoard from './Components/GameBoard.jsx';
 import Log from './Components/Log.jsx';
 
+function derivweActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+      if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+        currentPlayer = 'O';
+      }
+      return currentPlayer; 
+}
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+  // const [activePlayer, setActivePlayer] = useState('X');
 
+  const activePlayer = derivweActivePlayer(gameTurns);
+   
   function handleSelectedSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
     setGameTurns(prevturns => {
-      let currentPlayer = 'X';
-
-      if (prevturns.length > 0 && prevturns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
+     
+      const currentPlayer = derivweActivePlayer(prevturns);
 
       const updateTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer },
       ...prevturns];
--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -31,7 +31,8 @@ export default function GameBoard({ onSelectSquare, turns }) {
             <ol>
                 {row.map((playerSymbol, colIndex) => (
                 <li key={colIndex}>
-                    <button onClick={() =>onSelectSquare(rowIndex, colIndex)}>{playerSymbol}
+                    <button onClick={() =>onSelectSquare(rowIndex, colIndex)} 
+                    disabled={playerSymbol !== null}>{playerSymbol}
                     </button>
                     </li>
                 ))}
